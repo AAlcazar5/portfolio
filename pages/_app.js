28282e64905299926372f2213518a9eb33d47fa7
alex <aalcazar5@protonmail.com>
2022-11-01 19:21:56 -0700

    ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.scss";
 import "../styles/tailwind.scss";
 import { ThemeProvider } from "next-themes";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }) {
       storageKey="nightwind-mode"
       defaultTheme="system"
     >
+      <GoogleAnalytics trackPageViews strategy="lazyOnload" />
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -32,24 +32,6 @@ export default class MyDocument extends Document {
             content="A personal portfolio built using NextJS & TailwindCSS"
           />
           <meta property="og:image" key="ogimage" content="" />
-
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
         </Head>
         <body>
           <Main />
